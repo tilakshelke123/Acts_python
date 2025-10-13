0a279c2fe161eca6c4dbb88916aeffe1d38b3f56
tilakshelke123 <tilakshelke123456@gmail.com>
2025-10-13 17:44:44 +0530

    Update Exercise3.py

--- Assignment_2/Exercise3.py
+++ Assignment_2/Exercise3.py
@@ -1,8 +1,9 @@
 
-def fuel_cost (distaance,km_per_litre,cost_per_litre ):
+def fuel_cost (distaance,km_per_litre =50,cost_per_litre=60 ):
    
    return  (distaance/km_per_litre)*cost_per_litre
 
 distaance= int(input("Enter the distaance :-"))
-result = fuel_cost(distaance,km_per_litre = 20,cost_per_litre =30)
+result = fuel_cost(distaance)
+
 print("the cost of fuel cost is :-",result)
