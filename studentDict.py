a736b918a20ebba17d45e95659f31a7d9efc50c2
tilakshelke123 <tilakshelke123456@gmail.com>
2025-12-01 18:16:35 +0530

    Add files via upload

---
+++ studentDict.py
@@ -0,0 +1,15 @@
+
+list_Dict = [
+             {"rollno":"1","name":"Tilak","course":"ccst"},
+             {"rollno":"2","name":"manoj","course":"ccst"},
+             {"rollno":"3","name":"piyush","course":"ccst"},
+             {"rollno":"4","name":"vish","course":"ccst"},
+             {"rollno":"5","name":"vivek","course":"ccst"},
+             {"rollno":"6","name":"mrunali","course":"ccst"},
+             {"rollno":"7","name":"pragati","course":"ccst"}
+             ]
+print(list_Dict[1])
+print(list_Dict[2])
+print(list_Dict[3])
+print(list_Dict[4])
+print(list_Dict[5])
